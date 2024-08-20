6a5fd13b3efacf098dfef0f975141da8c1b2bc97
Abdullah-Kz <romantech2@gmail.com>
2024-08-20 07:48:10 -0700

    .

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,9 +1,30 @@
-
+import { useState } from 'react'
+import './app.css'
 function App() {
-
+let [counter , setcounter] =  useState(15)
+const addvalue = () => {
+  if(counter < 20 ){
+  setcounter(counter + 1)
+  }else{
+    alert('Value cannot be greater than 20')
+  }
+}
+const decreasevalue = () => {
+  if(counter === 0){
+    alert('Value cannot be less than 0')
+  }else{
+  setcounter(counter - 1)
+  }
+}
   return (
     <>
-    <div>Hello World</div>
+    <div id = 'app'>
+    <h1>This is a Counter</h1>
+    <h2>Counter value is {counter}</h2>
+    <button id = "button" onClick = {addvalue}>Click me to increase the value {counter}</button>
+    <br/>
+    <button id = "button" onClick = {decreasevalue}>Click me to decrease the value {counter}</button>
+    </div>
     </>
   )
 }
